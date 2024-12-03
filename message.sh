6d4f4e82c3840bf1584e0a0a2cae22c8e705d123
EC2 Default User <ec2-user@ip-172-31-24-212.ec2.internal>
2024-12-03 02:54:29 +0000

    bash script files

---
+++ message.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name =Brad
+echo "Hello my name is Meerim"
+echo "Meerim loves linux"
+echo "Meerim currently studing Devops "
+
---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir kiazen
+touch bash
+echo Hello world > hello world
+echo Bash is cool >> hello
+sudo useradd tim
+chmod 666 kaizen
+chown tim bash
+sed -i 's/cool/great/'hello
+sudo yum install httpd -y > /dev/null
